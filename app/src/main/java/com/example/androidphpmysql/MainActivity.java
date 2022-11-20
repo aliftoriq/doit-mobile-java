50f39e7d55baa08fde4d11630d40ae7b84815e55
aliftoriq <aliftoriq52@gmail.com>
2022-11-20 21:07:37 +0700

    selesai

---
+++ app/src/main/java/com/example/androidphpmysql/MainActivity.java
@@ -0,0 +1,113 @@
+package com.example.androidphpmysql;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    private EditText editTextUsername, editTextEmail, editTextPassword;
+    private Button buttonRegister;
+    private ProgressDialog progressDialog;
+
+    private TextView textViewLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        editTextUsername = (EditText) findViewById(R.id.editTextUsername);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+
+        textViewLogin = (TextView) findViewById(R.id.textViewLogin);
+
+        buttonRegister = (Button) findViewById(R.id.buttonRegister);
+
+        progressDialog = new ProgressDialog(this);
+
+        buttonRegister.setOnClickListener(this);
+        textViewLogin.setOnClickListener(this);
+    }
+
+    private void registerUser() {
+        final String email = editTextEmail.getText().toString().trim();
+        final String username = editTextUsername.getText().toString().trim();
+        final String password = editTextPassword.getText().toString().trim();
+
+        progressDialog.setMessage("Registering user...");
+        progressDialog.show();
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST,
+                Constants.URL_REGISTER,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        progressDialog.dismiss();
+
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+
+                            Toast.makeText(getApplicationContext(), jsonObject.getString("message"), Toast.LENGTH_LONG).show();
+
+                        } catch (JSONException e) {
+                            progressDialog.setMessage(response+e.toString());
+                            progressDialog.show();
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        progressDialog.hide();
+                        Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("username", username);
+                params.put("password", password);
+                params.put("email", email);
+                return params;
+            }
+        };
+
+
+        RequestHandler.getInstance(this).addToRequestQueue(stringRequest);
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == buttonRegister)
+            registerUser();
+        if (view == textViewLogin)
+            startActivity(new Intent(this, LoginActivity.class));
+    }
+}
